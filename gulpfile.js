d372a65352cb1191477382e7cc1493a849a08dad
JesterXL <jesse@jessewarden.com>
2015-07-18 12:31:59 -0400

    changed up gulp to use babelify for now

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,10 @@
-var gulp = require("gulp");
-var babel = require("gulp-babel");
-
-gulp.task("default", function () {
-  return gulp.src("src/app.js")
-    .pipe(babel())
-    .pipe(gulp.dest("dist"));
-});
-/*
 var fs = require("fs");
 var browserify = require("browserify");
 var babelify = require("babelify");
 
 browserify({ debug: true })
   .transform(babelify)
-  .require("./script.js", { entry: true })
+  .require("./src/app.js", { entry: true })
   .bundle()
   .on("error", function (err) { console.log("Error: " + err.message); })
   .pipe(fs.createWriteStream("bundle.js"));
-  */
--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 import Cycle from '@cycle/core';
 import CycleWeb from '@cycle/web';
 
-function main()
-{
-	console.log("main");
+function main() {
+  return {
+    DOM: Cycle.Rx.Observable.interval(1000)
+      .map(i => CycleWeb.h(
+        'h1', '' + i + ' seconds elapsed'
+      ))
+  };
 }
 
 let drivers = {
