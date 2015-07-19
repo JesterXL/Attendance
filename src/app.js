5d98a2c07450a62fe04e102938effd56db30d175
JesterXL <jesse@jessewarden.com>
2015-07-19 17:40:11 -0400

    making first url request

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ import Cycle from '@cycle/core';
 import {makeDOMDriver, hJSX} from '@cycle/dom';
 
 function main(responses) {
+
+  let click$ = responses.DOM.get('getPeopleButton', 'click');
+
   let requests = {
     DOM: responses.DOM.get('paper-checkbox', 'change')
       .map(function(ev)
@@ -14,6 +17,8 @@ function main(responses) {
         <div>
           <h2>toggled: {toggled ? 'true' : 'false'}</h2>
           <paper-checkbox toggled={toggled}>{toggled ? 'ON' : 'Off'}</paper-checkbox>
+          <div class="list short" id="peopleList"></div>
+          <paper-button raised id="getPeopleButton">Get People</paper-button>
         </div>
       )
   };
